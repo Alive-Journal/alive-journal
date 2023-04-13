d21fcc336c32e91ccc5db15378e7688a064819b5
Kyle Freemantle <kcfreemantle@gmail.com>
2023-04-13 12:11:08 -0700

    1st version of Users list set to models/users/model.js

---
+++ src/auth/models/users/model.js
@@ -0,0 +1,62 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../../config/connection");
+const bcrypt = require("bcryptjs");
+
+class User extends Model {
+  // Instance method to check if the entered password matches the stored password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+User.init(
+  {
+    // Define attributes for the User model
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+  },
+  {
+    hooks: {
+      // Hash the password before storing in the database
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
+);
+
+module.exports = User;
